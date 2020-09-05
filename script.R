41d7d0267e7b1959a7c9e921edd18ad501c9f997
Renan Chun <rechun@usp.br>
2020-09-05 11:14:10 -0300

    load data set

---
+++ script.R
@@ -0,0 +1,7 @@
+library(MASS) #load library
+Boston # dataset
+?Boston # info of boston dataSet
+#in boston description this data set has 506 rows and 14 columns  and they represent the housing values in suburbs of boston
+
+
+
