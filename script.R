06966fb2bd547d005415519d842237de850acf85
Renan Chun <rechun@usp.br>
2020-09-05 11:24:35 -0300

    item c

--- script.R
+++ script.R
@@ -13,3 +13,20 @@ pairs(Boston)
 
 #in the graph we can find that the high radio frequency index of accessibility to radial highways contains the highest level of per capita crime rate per city
 #we can also see that medv is inversely proportional to lstat- lower status of the population, nox - nitrogen oxides concentration and indus - proportion of non-retail businnes acres per town and directly proportional to rm- average number of rooms per dewlling
+
+#item c
+# in this case we need calculate the indexes of correlations between crime rates and other variables.
+
+Boston.corr = cor(Boston)
+Boston.corr.crim = Boston.corr[-1,1]
+
+print(
+  Boston.corr.crim[order(abs(Boston.corr.crim), decreasing = T)]
+)
+
+par(mfrow=c(2,2))
+# get the four most correlated variables
+aux = names(Boston.corr.crim[order(abs(Boston.corr.crim), decreasing = T)][1:4])
+for(i in aux){
+  plot(get(i), crim, xlab=i)
+}
