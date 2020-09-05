4d9274fe66f445e1e83c859830e6723a3ea40768
Renan Chun <rechun@usp.br>
2020-09-05 11:22:08 -0300

    item b

--- script.R
+++ script.R
@@ -1,7 +1,15 @@
 library(MASS) #load library
+#item A
 Boston # dataset
 ?Boston # info of boston dataSet
-#in boston description this data set has 506 rows and 14 columns  and they represent the housing values in suburbs of boston
 
+dim(Boston)
+#The rows represent observations of the U.S. Census Tracts in the Boston Area. The columns presents the measures of the Census Variables.
 
+#item b
+attach(Boston)
 
+pairs(Boston)
+
+#in the graph we can find that the high radio frequency index of accessibility to radial highways contains the highest level of per capita crime rate per city
+#we can also see that medv is inversely proportional to lstat- lower status of the population, nox - nitrogen oxides concentration and indus - proportion of non-retail businnes acres per town and directly proportional to rm- average number of rooms per dewlling
